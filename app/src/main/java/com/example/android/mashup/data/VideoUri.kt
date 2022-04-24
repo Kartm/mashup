8d527d2e32f525756f22545dcd006942d5690a02
Jakub Seredyński <53041150+sereczeq@users.noreply.github.com>
2022-04-24 10:54:22 +0200

    Added scripts for loading video uri

---
+++ app/src/main/java/com/example/android/mashup/data/VideoUri.kt
@@ -0,0 +1,12 @@
+package com.example.android.mashup.data
+
+import android.net.Uri
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "video_uri_table")
+data class VideoUri (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val uri: Uri
+)
