536c027ce050f5f17ad6efd80cd9a734356c31ed
Marcin Kasperski <marcin.kspr@gmail.com>
2022-05-16 11:06:53 +0200

    try 3: removed the navigation that was apparently so devastating

--- app/src/main/java/com/example/android/mashup/Details/DetailsFragment.kt
+++ app/src/main/java/com/example/android/mashup/Details/DetailsFragment.kt
@@ -6,10 +6,6 @@ import android.view.*
 import android.widget.Toast
 import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.mashup.R
 import com.example.android.mashup.databinding.FragmentDetailsBinding
@@ -36,10 +32,6 @@ class DetailsFragment : Fragment() {
         binding.description.text = args.video.description
         binding.title.text = args.video.title
         binding.thumbnail2.setImageBitmap(args.video.thumbnail)
-
-        binding.thumbnail2.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_detailsViewFullscreen)
-        }
 
         setHasOptionsMenu(true)
 
