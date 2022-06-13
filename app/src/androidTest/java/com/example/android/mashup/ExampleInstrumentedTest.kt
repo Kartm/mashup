2a1d0c323bee2ef7f13a7bc1c93aaf1259ac81eb
Marcin Kasperski <marcin.kspr@gmail.com>
2022-06-13 09:33:55 +0200

    managed to write a test

--- app/src/androidTest/java/com/example/android/mashup/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/android/mashup/ExampleInstrumentedTest.kt
@@ -1,12 +1,31 @@
 package com.example.android.mashup
 
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.launchFragment
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.TypeTextAction
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.mashup.Creator.CancelDialogFragment
+import com.example.android.mashup.Creator.CreatorFragment
+import com.example.android.mashup.Creator.SaveDialogFragment
+import com.example.android.mashup.Details.DetailsFragment
+import com.example.android.mashup.Feed.FeedFragment
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +33,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class   ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.android.mashup", appContext.packageName)
+    fun testCancelNavigation() {
+        // Create a mock NavController
+        val mockNavController = mock(NavController::class.java)
+
+        // Create a graphical FragmentScenario for the TitleScreen
+        val titleScenario = launchFragment<CancelDialogFragment>()
+
+        // Set the NavController property on the fragment
+        titleScenario.onFragment { fragment ->
+            Navigation.setViewNavController(fragment.requireView(), mockNavController)
+        }
+
+        // Verify that performing a click prompts the correct Navigation action
+        onView(ViewMatchers.withId(R.id.cancel_dialog_discard_button)).perform(ViewActions.click())
+        verify(mockNavController).navigate(R.id.action_cancelDialogFragment_to_FirstFragment2)
     }
 }
