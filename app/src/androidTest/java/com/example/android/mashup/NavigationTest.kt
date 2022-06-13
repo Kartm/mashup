04ecbcfb893a0e981e2875425cb077cc86e9964e
Marcin Kasperski <marcin.kspr@gmail.com>
2022-06-13 10:04:34 +0200

    fixed

--- app/src/androidTest/java/com/example/android/mashup/NavigationTest.kt
+++ app/src/androidTest/java/com/example/android/mashup/NavigationTest.kt
@@ -1,34 +1,26 @@
 package com.example.android.mashup
 
-import androidx.core.os.bundleOf
 import androidx.fragment.app.testing.launchFragment
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.testing.TestNavHostController
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.TypeTextAction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.mashup.Creator.CancelDialogFragment
-import com.example.android.mashup.Creator.CreatorFragment
-import com.example.android.mashup.Creator.SaveDialogFragment
-import com.example.android.mashup.Details.DetailsFragment
-import com.example.android.mashup.Feed.FeedFragment
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-import org.mockito.ArgumentMatchers
 import org.mockito.Mockito.*
 
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
 @RunWith(AndroidJUnit4::class)
-class NavigationTest {
+class   ExampleInstrumentedTest {
     @Test
     fun testCancelNavigation() {
         // Create a mock NavController
